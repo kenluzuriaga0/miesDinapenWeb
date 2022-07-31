6671b55f636584198e481d41aec97591652ea21e
Ken Luzuriaga <kenluzuriaga0@hotmail.com>
2022-07-31 04:11:21 -0500

    BACK Fix tipo de datos Organizacion de models.Organizacion y no schemas.Organizacion

--- miesDinapenApiRest/routes/organizaciones.py
+++ miesDinapenApiRest/routes/organizaciones.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter,Depends
 from config.db import get_db
 from sqlalchemy.orm import Session
-from models.models import Organizaciones
+from models import models
 from schemas.schemas import Organizaciones
 
 router = APIRouter()
@@ -9,6 +9,6 @@ router = APIRouter()
 
 @router.get('/api/organizaciones', response_model=list[Organizaciones])
 def addOrganizaciones(db:Session = Depends(get_db)):
-    myorganizaciones:Organizaciones = db.query(Organizaciones).all()
+    myorganizaciones = db.query(models.Organizaciones).all()
     return myorganizaciones
  
